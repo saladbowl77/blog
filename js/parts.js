dcaace156557120b917da791807868c8c0140a23
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2020-08-05 20:51:02 +0900

    v2

--- js/parts.js
+++ js/parts.js
@@ -1,30 +1,42 @@
 var xhrh = new XMLHttpRequest(),
   method = "GET",
   url = "/common.html";
+var xhrb = new XMLHttpRequest(),
+  method = "GET",
+  url = "/common.html";
 var xhrf = new XMLHttpRequest(),
   method = "GET",
   url = "/common.html";
 var header = document.getElementsByTagName("header")[0];
+var bottom_menu_wrap = document.getElementById("bottom_menu_wrap");
 var footer = document.getElementsByTagName("footer")[0];
-
-xhrh.responseType="document";//XMLとして扱いたいので一応記述
+xhrh.responseType="document";
 xhrh.open(method, url, true);
 xhrh.onreadystatechange = function () {
   if(xhrh.readyState === 4 && xhrh.status === 200) {
-    var restxt=xhrh.responseXML;//重要
-    var int=restxt.getElementsByTagName("header")[0];//読み込まれるセレクタを指定
-    header.innerHTML=int.outerHTML;//完了
+    var restxt=xhrh.responseXML;
+    var int=restxt.getElementsByTagName("header")[0];
+    header.innerHTML=int.outerHTML;
   }
 };
 xhrh.send();
-
-xhrf.responseType="document";//XMLとして扱いたいので一応記述
+xhrb.responseType="document";
+xhrb.open(method, url, true);
+xhrb.onreadystatechange = function () {
+  if(xhrb.readyState === 4 && xhrb.status === 200) {
+    var restxt=xhrb.responseXML;
+    var int=restxt.getElementById("bottom_menu");
+    footer.innerHTML=int.outerHTML;
+  }
+};
+xhrb.send();
+xhrf.responseType="document";
 xhrf.open(method, url, true);
 xhrf.onreadystatechange = function () {
   if(xhrf.readyState === 4 && xhrf.status === 200) {
-    var restxt=xhrf.responseXML;//重要
-    var int=restxt.getElementsByTagName("footer")[0];//読み込まれるセレクタを指定
-    footer.innerHTML=int.outerHTML;//完了
+    var restxt=xhrf.responseXML;
+    var int=restxt.getElementsByTagName("footer")[0];
+    footer.innerHTML=int.outerHTML;
   }
 };
 xhrf.send();
